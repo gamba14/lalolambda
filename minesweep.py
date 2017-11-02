9fc2ae87a164e1149bf1583dfd90d2da41653ae3
Agustin <agustin@S-L845.lan>
2017-11-02 12:26:46 -0300

    prototipo de funciones y definicion basica del tablero

---
+++ minesweep.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os.path
+import sys, getopt
+import random
+import time
+
+def randomer(arg):
+    pass
+
+def initTablero(tamanio):
+    #inicializa el tablero
+    tablero = [['0' for i in range(tamanio)] for i in range(tamanio)]
+    return (tablero)
+def minarTablero(numMinas,tamanio):
+    
+
+
+#Jugar funcion que me permite basicamente jugar
+def jugar():
+    pass
+
+if __name__ == '__main__':
+    jugar()
