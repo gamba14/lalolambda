45ea2c62011597b9dd243dd08daff8637aecb483
Agustin <agustin@S-L845.lan>
2017-11-07 12:25:20 -0300

    Juego casi funcional, pulir detalles

--- minesweep.py
+++ minesweep.py
@@ -4,6 +4,7 @@ import os.path
 import sys, getopt
 from random import randint
 import time
+import string
 
 def randomer(tamanio,numMinas):
     celdaA = randint(1,tamanio-1) #a veces fallaba con 0,tamanio
@@ -33,9 +34,26 @@ def getVecinos(tableroMinado,f,c):
                 if tableroMinado[f+i][c+j] == 1:
                     vecinos.append((f+i,c+j))
     return vecinos
+def descubrirMina(tableroMinado,tablero,f,c):
+    if tableroMinado[f][c] == 1:
+        print('BOOOOOOOOOOOOOOOOOOM\n')
+        jugarNuevo()
+    else:
+        vecinos = getVecinos(tableroMinado,f,c)
+        tablero[f][c]= len(vecinos)
+        for i in vecinos:
+            if tableroMinado[i[0]][i[1]] != 1:
+                tablero[i[0]][i[1]]=-1
+    return tablero
+def jugarNuevo():
+    continuar=input('Jugar de nuevo?(s/n)\n')
+    if continuar.lower() == 's':
+        return True
+    else:
+        return False
 
 def mostrarTablero(tableroMinado):
-    os.system('clear')
+    #os.system('clear')
     tamanio = len(tableroMinado)
     #imprimo una linea horizontal del ancho de la grilla
     horizontal = '   ' + (4 * tamanio * '-') + '-'
@@ -43,21 +61,28 @@ def mostrarTablero(tableroMinado):
     for i in enumerate(tableroMinado):
         print(i)
     print(horizontal)
+def parseInput():
+    dictCo={'a':0,'b':1,'c':2,'d':3,'e':4}
+    cor = input('>ingrese coordenada. ')
+    corList = list(cor)
+    corListPar=(dictCo[corList[0]],int(corList[1]))
+    return corListPar
 
 #Jugar funcion que me permite basicamente jugar
-def jugar(minas, tamanio):
+def jugar(minas, tamanio,tablero):
     perdiste = False
     tableroMinado = minarTablero(minas,tamanio)
-    #while not perdiste:
-    mostrarTablero(tableroMinado)
-#    for i in range(len(tableroMinado)):
-#        for j in range(len(tableroMinado)):
-#            vecinos = getVecinos(tableroMinado,i,j)
-    vecinos = getVecinos(tableroMinado,3,3)
-    print(vecinos)
+    while not perdiste:
+        coordenada=parseInput()
+        tablero = descubrirMina(tableroMinado,tablero,coordenada[0],coordenada[1])
+        mostrarTablero(tableroMinado)
+        vecinos = getVecinos(tableroMinado,3,3)
+        print(vecinos)
+        mostrarTablero(tablero)
 
 if __name__ == '__main__':
     #tamanio = input('Ingrese tamanio (0..9)')
     #minas = input('Ingrese cantidad de minas')
     #jugar(minas, tamanio)
-    jugar(3,6)
+    tablero=initTablero(6)
+    jugar(10,6,tablero)
